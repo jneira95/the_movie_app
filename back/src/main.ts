a5c84b3d8b954d23b8004fff4bfa13e569602449
Jorge Neira <jorgengarriga@gmail.com>
2024-11-30 18:56:51 +0100

    feat(back): add @nestjs/axios; update main.ts for NestExpressApplication

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,11 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('port');
   await app.listen(port);
 }
+
 bootstrap();
